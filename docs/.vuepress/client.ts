bcecc27e8ad9a2ec9cde4766d32a4b05bac636a9
BTMuli <bt-muli@outlook.com>
2025-08-23 18:07:13 +0800

    ✨ vp-card添加rybbit track

--- docs/.vuepress/client.ts
+++ docs/.vuepress/client.ts
@@ -1,11 +1,13 @@
 import { defineClientConfig } from "vuepress/client";
 
+import VPCard from "./components/VPCard.js";
 import Bilibili from "./components/Bilibili.js";
 import Sponsor from "./components/Sponsor.vue";
 import SponsorList from "./components/SponsorList.vue";
 
 export default defineClientConfig({
   enhance({ app }) {
+    app.component("VPCard", VPCard);
     app.component("Bilibili", Bilibili);
     app.component("Sponsor", Sponsor);
     app.component("SponsorList", SponsorList);
